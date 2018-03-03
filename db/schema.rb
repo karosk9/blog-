bc0a74366828b381d5a30d8a094d5c4a3a02c113
Karolina <karosk9@gmail.com>
2018-03-03 15:30:48 +0100

    add constraints to comments for body

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303141921) do
+ActiveRecord::Schema.define(version: 20180303142732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20180303141921) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter", null: false
-    t.text "body"
+    t.text "body", null: false
     t.bigint "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
