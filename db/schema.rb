1552f2b59439804fed0232c15bf9011ff84b9e95
Karolina <karosk9@gmail.com>
2018-03-03 15:21:29 +0100

    add constraints to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180218100009) do
+ActiveRecord::Schema.define(version: 20180303141921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
+    t.string "commenter", null: false
     t.text "body"
     t.bigint "article_id"
     t.datetime "created_at", null: false
