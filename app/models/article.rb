56b2db751e83b55dbc7be731507d7680e8f154f1
Karolina <karosk9@gmail.com>
2018-03-04 12:17:26 +0100

    Add tags to article model and show action

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,15 @@
 class Article < ApplicationRecord
 		validates :title, :text, presence: true, length: { minimum: 5 }
-		has_many :comments, dependent: :destroy 
+		has_many :comments, dependent: :destroy
+
+		def tag=(text)
+			text.is_a?(String) ? super(sanitize_tags(text)) : super(text)
+# jeśli text jest stringiem wywołuje metodę sanitize tags która zmienia string na array, jeśli nie jest stringiem nie wykona się 
+		end
+
+		private
+
+		def sanitize_tags(text)
+			text.split(" ").map(&:downcase).uniq
+		end
 end
